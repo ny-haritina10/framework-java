12e2fe9207062ff54a43d86c4108e11cd2b55fab
DaxNH10 <daxnh10@gmail.com>
2024-05-16 20:39:37 +0100

    sprint1-etu2716
    Commit sprint1 etu2716

--- src/java/FrontController.java
+++ src/java/FrontController.java
@@ -1,11 +1,40 @@
 package controller;
 
 import java.io.*;
+import java.util.List;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
+import javax.servlet.ServletContext;
+
+import utils.*;
 
 public class FrontController extends HttpServlet {
+
+    private String controllerPackage;
+    private ControllerScanner scanner;
+    private List<Class<?>> controllers;
+
+    @Override
+    public void init(ServletConfig config) 
+        throws ServletException 
+    {
+        try {
+            super.init(config);
+
+            ServletContext context = config.getServletContext();
+            controllerPackage = context.getInitParameter("base_package");
+
+            this.scanner = new ControllerScanner();
+            this.controllers = scanner.findControllers(controllerPackage);
+        } 
+        
+        catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
+
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException
@@ -29,7 +58,11 @@ public class FrontController extends HttpServlet {
             
             out.println("<h1>" + " Hello World " + "</h1>");
             out.println("<h2>" + " You are here now: " + "</h2>");
-            out.println("<h3> URL: " + url + "</h3>");
+            out.println("<h3> URL: " + url + " </h3>");
+
+            for (Class<?> controller : this.controllers) {
+                out.println("Found controller: " + this.controller.getName() + "<br>");
+            }
         } 
         
         catch (Exception e) {
